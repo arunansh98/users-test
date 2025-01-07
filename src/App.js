bcebf70bf95901b961bad9491db0a309dc3b1135
Arunansh Srivastava <arunansh98@gmail.com>
2025-01-07 11:48:55 +0530

    Initial Project Setup

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import UserForm from "./UserForm";
+import UserList from "./UserList";
+
+export default function App() {
+  const [users, setUsers] = useState([]);
+
+  const onUserAdd = (user) => {
+    setUsers([...users, user]);
+  };
+
+  return (
+    <div>
+      <UserForm onUserAdd={onUserAdd} />
+      <hr />
+      <UserList users={users} />
+    </div>
+  );
+}
